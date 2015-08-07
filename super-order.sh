591b12471cffe59acc277081f2735cd8ff8bf871
root <beikejinmiao@gmail.com>
2015-08-07 13:09:20 +0800

    super orders

--- super-order.sh
+++ super-order.sh
@@ -5,3 +5,8 @@
 # 1.在指定目录下递归替换所有文件中的内容
 ## 使用时按需求替换命令中的内容
 grep -r -l  'old_content' directory_name/ | xargs  sed -i 's/old_content/new_content/g'
+
+
+# 2.如果想要替换的内容中有'/'符号
+## 可以使用其他符号代替,比如'#'
+sed -i 's#old_content#new_content#g'
